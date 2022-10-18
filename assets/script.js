b545f0a74a2e04ebd636b53db83207e1c1d6d523
Z. R <zmirza7701@gmail.com>
2022-10-18 14:29:07 -0500

    added time block and save button

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,7 @@
-var today = moment().format("dddd, MMMM Do YYYY, h:mm.ss")
+var today = moment().format("dddd, MMMM Do YYYY, h:mm:ss")
 $("#currentDay").text(today);
+
+
+const saveBtn = $("#save");
+
+document.getElementById("textBox").value = localStorage.getItem("9AM");
