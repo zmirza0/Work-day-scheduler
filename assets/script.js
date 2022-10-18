1f120c5e61d545d9aa051143a4c0e0ea5fe2208a
Z. R <zmirza7701@gmail.com>
2022-10-18 14:44:33 -0500

    added save functionality with local storage for 9am

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 var today = moment().format("dddd, MMMM Do YYYY, h:mm:ss")
 $("#currentDay").text(today);
 
-
 const saveBtn = $("#save");
 
-document.getElementById("textBox").value = localStorage.getItem("9AM");
+document.getElementById("textbox9am").value = localStorage.getItem("9amnote");
+
+saveBtn.on('click', function () {
+    localStorage.setItem("9amnote", document.getElementById("textbox9am").value)
+    console.log(document.getElementById("textbox9am").value)
+  });
